1bc65be6c28430f332bd3417867bc0b53f9fcd82
igor2000xp <igorring@gmail.com>
2022-11-13 13:02:00 +0300

    feat: add and setup eslint and prettier

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: '/navigator?tab=0', pathMatch: 'full' },
+  // { path: '/navigator?tab=0', component: TabPage },
+  // { path: '/navigator?tab=1', component: TabPage },
+  // { path: '/navigator?tab=2', component: TabPage },
+  // { path: '/navigator?tab=r', component: TabPage },
+  // { path: '**', component: Page404Component },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
