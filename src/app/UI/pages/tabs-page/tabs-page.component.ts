84ad03001382124fae3e6823d6f11b31a41f614b
igor2000xp <igorring@gmail.com>
2022-11-14 05:31:37 +0300

    feat: implement draft tabs-page and 404 page add files

---
+++ src/app/UI/pages/tabs-page/tabs-page.component.ts
@@ -0,0 +1,53 @@
+import { Component, OnInit } from '@angular/core';
+
+export interface IContentPageElement {
+  name: string;
+  account: number;
+}
+// export type IActivePage = 'active' || '';
+export const activePageInit: Array<'active' | ''> = ['', '', '', ''];
+
+@Component({
+  selector: 'app-tabs-page',
+  templateUrl: './tabs-page.component.html',
+  styleUrls: ['./tabs-page.component.scss'],
+})
+export class TabsPageComponent implements OnInit {
+  private isActiveTab: string = '0';
+
+  public contentPage: IContentPageElement[] = [{ name: '', account: 0 }];
+
+  public activePage: Array<'active' | ''> = ['active', '', '', ''];
+
+  // constructor() {}
+  //
+  private switchPage(idPage: string) {
+    this.activePage = activePageInit;
+    switch (idPage) {
+      case '0':
+        this.contentPage = [{ name: 'Zero Page', account: 0 }];
+        this.activePage = ['active', '', '', ''];
+        break;
+      case '1':
+        this.contentPage = [{ name: 'First Page', account: 1 }];
+        this.activePage = ['', 'active', '', ''];
+        break;
+      case '2':
+        this.contentPage = [{ name: 'Second Page', account: 2 }];
+        this.activePage = ['', '', 'active', ''];
+        break;
+      case '3':
+        this.contentPage = [{ name: 'Third Page', account: 3 }];
+        this.activePage = ['', '', '', 'active'];
+        break;
+      default:
+        this.contentPage = [{ name: 'Something goes wrong', account: 100 }];
+    }
+  }
+
+  ngOnInit(): void {
+    this.isActiveTab = document.location.search.split('=')[1];
+    this.switchPage(this.isActiveTab);
+    // console.log(idPage);
+  }
+}
